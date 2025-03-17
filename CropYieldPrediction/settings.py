f435146b7beacd909f7a81246f1ce48ae4fdc397
adithiyagit <adithiyavinu.b@gmail.com>
2025-03-17 13:11:25 +0530

    Update settings.py

--- CropYieldPrediction/settings.py
+++ CropYieldPrediction/settings.py
@@ -27,9 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
-    'https://papaerchase-esgbcmhwdvfghbc2.canadacentral-01.azurewebsites.net'
+    "https://polynomialprediction-bbedb2b6gwdyajhf.centralus-01.azurewebsites.net",
 ]
-
 
 # Application definition
 
